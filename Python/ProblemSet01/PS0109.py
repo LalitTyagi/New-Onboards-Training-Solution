a1e60f2d1e88b9245fe0e28de29ed8f15530dc41
Lalit Tyagi <luckytyagi61@gmail.com>
2018-06-19 16:22:52 +0530

    Create PS0109.py

---
+++ Python/ProblemSet01/PS0109.py
@@ -0,0 +1,7 @@
+width = 17
+height = 12.0
+
+print type(width / 2)    # <type 'int'>
+print type(width / 2.0)  # <type 'float'>
+print type(height / 3)   # <type 'float'>
+print type(1 + 2 * 5)    # <type 'int'>
